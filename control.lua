c5ccce81f104decbfd62436aa08e91f8059b3fed
Dobberoonski <versericaw@gmail.com>
2025-08-14 09:56:05 +1000

    msg

--- control.lua
+++ control.lua
@@ -1,1 +1,44 @@
 --control.lua
+local function modulePartyInit()
+    if not storage.moduleparty then
+        storage.moduleparty = {}
+    end
+    if not storage.isRecipeSet then
+        storage.isRecipeSet = {}
+    end
+end
+
+script.on_init(modulePartyInit)
+script.on_configuration_changed(modulePartyInit)
+
+local function getRandomModule()
+    --getRandomModule : -> item
+    return {name="efficiency-module", count=1, quality="normal"}
+end
+
+script.on_event(defines.events.on_tick, function()--i'm sorry
+    storage.isRecipeSet = storage.isRecipeSet or {}
+    for _,v in pairs(storage.isRecipeSet) do
+        if storage.moduleparty[v].get_recipe() ~= nil then
+            storage.isRecipeSet[_] = nil
+            while(storage.moduleparty[v].get_module_inventory().is_full() == false) do
+                storage.moduleparty[v].get_module_inventory().insert(getRandomModule())
+            end
+        end
+    end
+end)
+
+script.on_event(defines.events.on_script_trigger_effect, function(event)
+    if event.effect_id ~= "get-ready-to-party" then return end
+    local key = script.register_on_object_destroyed(event.source_entity)
+    table.insert(storage.isRecipeSet, key)
+    storage.moduleparty[key] = event.source_entity
+end)
+
+script.on_event(defines.events.on_object_destroyed, function(event)
+    local key = event.registration_number
+    if not storage.moduleparty[key] then return end--required if Maraxsis 1.30.49 installed.
+    --see bug report: https://github.com/notnotmelon/maraxsis/issues/323
+    --tl;dr Maraxsis will call this function whenever **any** entity is created. Lmfao.
+    storage.moduleparty[key] = nil
+end)
--- data.lua
+++ data.lua
@@ -1,18 +1,26 @@
 --data.lua
---[[Ass1]]
-local ass1 = data.raw["assembling-machine"]["assembling-machine-1"]
-ass1.module_slots = 1--we do a bit of trolling.
+--[[assembling-machine-1]]
+data.raw["assembling-machine"]["assembling-machine-1"].module_slots = 1--we do a bit of trolling.
 
 --[[assembling-machine]]
 for k,v in pairs(data.raw["assembling-machine"]) do
     if #v.allowed_effects == 0 then return end
-    --if v.module_slots == 0 then v.module_slots = 1 end
     v.quality_affects_module_slots = true
     v.quality_slots_module_bonus = {
-        ["common"] = 0,
+        ["normal"] = 0,
         ["uncommon"] = 1,
         ["rare"] = 2,
         ["epic"] = 3,
         ["legendary"] = 5
+    }
+    v.created_effect = {
+        type = "direct",
+        action_delivery = {
+            type = "instant",
+            source_effects = {
+                type = "script",
+                effect_id = "get-ready-to-party"
+            }
+        }
     }
 end
