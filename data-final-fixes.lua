f3039eb97f1d0bbd2673e71dbd13b4222abf451f
Dobberoonski <versericaw@gmail.com>
2025-08-15 23:21:10 +1000

    uncommon and rare quality unlocked at beginning of game

---
+++ data-final-fixes.lua
@@ -0,0 +1,22 @@
+local qualityModuleTech = data.raw["technology"]["quality-module"]
+qualityModuleTech.prerequisites = nil
+qualityModuleTech.enabled = true
+qualityModuleTech.unit = nil
+qualityModuleTech.research_trigger = {
+    count = 1,
+    item = "iron-plate",
+    type = "craft-item"
+}
+
+local epicQualityTech = data.raw["technology"]["epic-quality"]
+epicQualityTech.prerequisites = {
+    "agricultural-science-pack",
+    "utility-science-pack",
+    "modules"
+}
+
+local qualityModuleTwoTech = data.raw["technology"]["quality-module-2"]
+qualityModuleTwoTech.prerequisites = {
+    "modules",
+    "space-science-pack"
+}
